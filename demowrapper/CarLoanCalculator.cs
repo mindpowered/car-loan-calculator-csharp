dc8808e9a2bdb75e2b5fc1913b4e5514d93fb4ac
Build <support@mindpowered.ca>
2020-11-21 18:52:53 +0000

    Version 0.0.3

--- demowrapper/CarLoanCalculator.cs
+++ demowrapper/CarLoanCalculator.cs
@@ -8,46 +8,47 @@
  * car loan
  * calculator
  */
-public class CarLoanCalculator
-{
+namespace wrappers {
+	public class CarLoanCalculator
 
-	/**
-	 * calc payments from price
-	 * @param newCarPrice 
-	 * @param tradeInAllowance 
-	 * @param tradeInLoanBalance 
-	 * @param downPaymentAndRebates 
-	 * @param loanDuration in months
-	 * @param salesTaxRate percent
-	 * @param interestRate percent
-	 * @return {number} The x value.
-	*/
-	CalcPayments(newCarPrice, tradeInAllowance, tradeInLoanBalance, downPaymentAndRebates, loanDuration, salesTaxRate, interestRate)
-	{
-		//TODO: C# let bus = maglev.maglev.MagLev.getInstance('default');
-		//TODO: C# let args = [newCarPrice, tradeInAllowance, tradeInLoanBalance, downPaymentAndRebates, loanDuration, salesTaxRate, interestRate];
-		//TODO: C# let ret = bus.call('CarLoanCalculator.CalcPayments', args);
-		//TODO: C# return ret;
+		/**
+		 * Calculate what the payments would be from the price of the new car and the parameters of the monthly loan payments
+		 * @param newCarPrice price of the car new
+		 * @param tradeInAllowance trade-in value
+		 * @param tradeInLoanBalance loan balance after trade-in
+		 * @param downPaymentAndRebates total amount of rebates plus downpayment
+		 * @param loanDuration loan duration in months
+		 * @param salesTaxRate  sales tax as percentage
+		 * @param interestRate interest rate as percentage
+		 * @return {number} The x value.
+		*/
+		CalcPayments(newCarPrice, tradeInAllowance, tradeInLoanBalance, downPaymentAndRebates, loanDuration, salesTaxRate, interestRate)
+		{
+			//TODO: C# let bus = maglev.maglev.MagLev.getInstance('carloancalculator');
+			//TODO: C# let args = [newCarPrice, tradeInAllowance, tradeInLoanBalance, downPaymentAndRebates, loanDuration, salesTaxRate, interestRate];
+			//TODO: C# let ret = bus.call('CarLoanCalculator.CalcPayments', args);
+			//TODO: C# return ret;
+		}
+
+		/**
+		 * Calculate the price of the car from the monthly loan payment information
+		 * @param monthlyPayment monthly payment amount
+		 * @param tradeInAllowance trade-in value
+		 * @param tradeInLoanBalance loan balance after trade-in
+		 * @param downPaymentAndRebates total amount of rebates plus downpayment
+		 * @param loanDuration loan duration in months
+		 * @param salesTaxRate sales tax rate as percentage
+		 * @param interestRate interest rate as percentage
+		 * @return {number} The x value.
+		*/
+		CalcAffordability(monthlyPayment, tradeInAllowance, tradeInLoanBalance, downPaymentAndRebates, loanDuration, salesTaxRate, interestRate)
+		{
+			//TODO: C# let bus = maglev.maglev.MagLev.getInstance('carloancalculator');
+			//TODO: C# let args = [monthlyPayment, tradeInAllowance, tradeInLoanBalance, downPaymentAndRebates, loanDuration, salesTaxRate, interestRate];
+			//TODO: C# let ret = bus.call('CarLoanCalculator.CalcAffordability', args);
+			//TODO: C# return ret;
+		}
+
 	}
-
-	/**
-	 * calc price from payment
-	 * @param monthlyPayment 
-	 * @param tradeInAllowance 
-	 * @param tradeInLoanBalance 
-	 * @param downPaymentAndRebates 
-	 * @param loanDuration in months
-	 * @param salesTaxRate percent
-	 * @param interestRate percent
-	 * @return {number} The x value.
-	*/
-	CalcAffordability(monthlyPayment, tradeInAllowance, tradeInLoanBalance, downPaymentAndRebates, loanDuration, salesTaxRate, interestRate)
-	{
-		//TODO: C# let bus = maglev.maglev.MagLev.getInstance('default');
-		//TODO: C# let args = [monthlyPayment, tradeInAllowance, tradeInLoanBalance, downPaymentAndRebates, loanDuration, salesTaxRate, interestRate];
-		//TODO: C# let ret = bus.call('CarLoanCalculator.CalcAffordability', args);
-		//TODO: C# return ret;
-	}
-
 }
 
